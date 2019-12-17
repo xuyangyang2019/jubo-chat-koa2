aa5efe8b247054fa89b4d647a8b4f1737732b861
xuyangyang2019 <15268296911@163.com>
2019-12-17 15:58:38 +0800

    index整合

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,25 @@
 // MVC Controllers
-const fs = require('fs');
+
+// 原始语法
+let fn_hello = async (ctx, next) => {
+    ctx.response.type = 'text/html';
+    let name = ctx.request.query.name || 'world';
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+};
+
+// Nunjucks 模板方法
+let fn_index = async (ctx, next) => {
+    ctx.render('index.html', {
+        title: 'Welcome'
+    });
+};
 
 /**
  * 异步读取文件
  * @param {*} url 
  */
 function read(url) {
+    const fs = require('fs');
     return new Promise((resolve, reject) => {
         fs.readFile(url, 'utf-8', function (err, data) {
             if (err) return reject(err);
@@ -14,16 +28,20 @@ function read(url) {
     })
 }
 
+let fn_vue = async (ctx, next) => {
+    let path = './views/indexVue.html'
+    // 构造解析 异步读取
+    const { status, body } = await read(path);
+    // 同步读取
+    // let status = 200
+    // let body = fs.readFileSync(path, 'utf-8');
+    ctx.state = status;
+    ctx.type = 'text/html';
+    ctx.body = body;
+}
+
 module.exports = {
-    'GET /': async (ctx, next) => {
-        let path = './dist/index.html'
-        // 构造解析 异步读取
-        const { status, body } = await read(path);
-        // 同步读取
-        // let status = 200
-        // let body = fs.readFileSync(path, 'utf-8');
-        ctx.state = status;
-        ctx.type = 'text/html';
-        ctx.body = body;
-    }
+    'GET /': fn_index,
+    'GET /vue': fn_vue,
+    'GET /hello/:name': fn_hello
 };
